4eb23073649322a527747a6c66da3bca33a85dac
jerryandersom <zohaibsaddiqui5@gmail.com>
2024-06-18 16:56:28 -0400

    files added

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MzA5OTEtem9oYWli@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MzIwOTQtem9oYWli@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
