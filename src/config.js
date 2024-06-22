1634b7747d5155f8dda3d6ce2fae7dea20f3ea19
jerryandersom <zohaibsaddiqui5@gmail.com>
2024-06-22 12:47:26 -0400

    production build is ready

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const API_URL = window.location.hostname === "localhost" ? "https://zohaibsaddiq-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://zohaibsaddiq-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
+// export const API_URL = window.location.hostname === "localhost" ? "https://zohaibsaddiq-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://zohaibsaddiq-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
+export const API_URL = window.location.origin;
 console.log(
     "API_URL :",
     API_URL
